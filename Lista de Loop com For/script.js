60d0b3297d7fdeb6ea6b6b913708b546441657b5
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2022-10-16 19:31:46 -0300

    iniciando primeira lista de atividade

---
+++ Lista de Loop com For/script.js
@@ -0,0 +1,21 @@
+// //ex 1
+// console.log('ex 1')
+// function renderUpToHundred () {
+//     for (let contadora = 0; contadora  <= 100; contadora++) {
+//         console.log(`${contadora}`)
+//     }
+// }
+
+// renderUpToHundred ()
+
+// //ex 2
+// console.log('ex 2')
+// function tenInTenToAHundred() {
+//     for (let cont = 10; cont <= 100; cont += 10) {
+//         console.log(`${cont}`)
+//     }
+// }
+
+// tenInTenToAHundred()
+
+//ex 3
