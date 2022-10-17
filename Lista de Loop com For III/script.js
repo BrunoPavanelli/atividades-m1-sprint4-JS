e303ea6e158d2da777cea6068f63a09e2d67e67f
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2022-10-17 20:21:42 -0300

    alteracao ex 2

--- Lista de Loop com For III/script.js
+++ Lista de Loop com For III/script.js
@@ -1,25 +1,31 @@
-//ex 1 Sequencia Fibonacci
-function Fibonacci() {
-    anterior = 0
-    atual = 1
-    for (let f = 1; f <= 15; f++) {
-        proximo = anterior + atual
-        anterior = atual
-        atual = proximo
-        console.log(`${anterior}`)
-    }
-}
-Fibonacci()
+// //ex 1 Sequencia Fibonacci
+// function Fibonacci() {
+//     anterior = 0
+//     atual = 1
+//     for (let f = 1; f <= 15; f++) {
+//         proximo = anterior + atual
+//         anterior = atual
+//         atual = proximo
+//         console.log(`${anterior}`)
+//     }
+// }
+// Fibonacci()
 
 //ex 2 
 function contaGraos() {
-    graosTotal = 1
-    graos = 1
-    for (let c = 1; c <= 64; c++) {
-        graosProximaCasa = graosTotal + graos * 2
-        graosTotal = graosProximaCasa
-        graos = graos*2
-        console.log(`${graosTotal}`)
-    }
+//     graosTotal = 1
+//     graos = 1
+//     for (let c = 1; c <= 64; c++) {
+//         graosProximaCasa = graosTotal + graos * 2
+//         graosTotal = graosProximaCasa
+//         graos = graos*2
+//         console.log(`${graosTotal}`)
+//     }
+// }
+    soma = 0
+    for (let c = 0; c <= 64; c++) {
+        soma += 2**c
+        console.log(`${soma}`)
+    }    
 }
 contaGraos()
