71ce6e8aa0f4f6f34b29386320468491d8524fb0
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2022-10-17 17:54:33 -0300

    feito ate o ex 2

---
+++ Desenvolvendo a manipulação de loops/script.js
@@ -0,0 +1,28 @@
+// //ex 1
+// function verticalText(String) {
+//     let contaLetras 
+//     for (c = 0; c < String.length; c++) {
+//         contaLetras = String[c]
+//         console.log(`${contaLetras}`)
+//     }
+// }
+// let palavra = prompt('Digite uma palavra')
+// verticalText(palavra)
+
+// //ex 2
+// function incrementText(string) {
+//     let contLetras = ''
+//     for (c = 0; c < string.length; c++) {
+//         contLetras += string[c]
+//         console.log(`${contLetras}`)
+//     }
+// }
+// let palavra2 = prompt('Digite uma palavra')
+// incrementText(palavra2)
+
+// //ex 3
+// function incrementTextBackWards(string) {
+
+// }
+// let palavra3 = prompt('Digite uma palavra')
+// incrementTextBackWards(palavra3)
