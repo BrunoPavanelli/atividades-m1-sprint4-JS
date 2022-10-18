5cd1ee29691d1e4bf32d031a78a2eb25295b2a1a
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2022-10-18 18:12:35 -0300

    ex 1 e 2 adicionados

---
+++ Lista de Array - I/script.js
@@ -0,0 +1,23 @@
+// //ex 1
+// function recebeValor() {
+//     let texto = prompt('Digite aqui sua palavra')
+//     while (texto.length < 5){
+//         alert('Minimo de 5 caracteres. Digite novamente')
+//         texto = prompt('Digite aqui sua palavra')
+//     }
+//     let arr_texto = [texto]
+//     return arr_texto
+// }
+// alert(`${recebeValor()}`)
+
+// //ex 2
+// let arr = ["Hermanoteu", "Olonéia", "Migalatéia", "Godart"]
+// function recebeNumero(x) {
+//     if (x < 0 || x > arr.length){
+//         alert('Valor nao encontrado')
+//     } else {
+//         alert(arr[x])
+//         return arr[x]
+//     } 
+// }
+// recebeNumero(5)
