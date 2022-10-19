ac36b200eb26272bf3d20325cd4ecc12d9b58138
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2022-10-19 16:21:05 -0300

    lista finalizada

--- Lista de Array - I/script.js
+++ Lista de Array - I/script.js
@@ -36,12 +36,12 @@
 // }
 // recebeInteiro(9)
 
-// //ex 4
+//ex 4
 // let nomes = ["12", "123", "123456789", '12345','12345678', '123456', '1234567'] 
 // function recebeLista(lista) {
 //     maiorNome = ''
-//     for (i = 0; i < lista.length; i++) {
-//         nomeAtual = lista[i]
+//     for (let i = 0; i < lista.length; i++) {
+//         let nomeAtual = lista[i]
 //         if (nomeAtual.length > maiorNome.length) {
 //             maiorNome = nomeAtual
 //         }
@@ -50,10 +50,88 @@
 // }
 // recebeLista(nomes)
 
-//ex 5
-let numeros = [1,4,6,9,11,8]
-function recebeValores(lista){
-    let n1 = lista.length
-    console.log(n1)
-}
-recebeValores(numeros)
+// //ex 5
+// let numeros = [1,4,6,9,11,8]
+// function recebeValores(lista){
+//     // let n1 = 0
+//     // let n2 = 0
+//     let somaNFinais = 0
+//     // for (let i = 0; i < lista.length; i++) {
+//     //     n1 = lista[i]
+//     // }
+//     // console.log(n1)
+//     // for (let i = 0; i < lista.length -1; i++){
+//     //     n2 = lista[i] 
+//     // }
+//     // console.log(n2) 
+//     for (let i = 0; i < lista.length; i++) {
+//         if (i > lista.length -3) {
+//             somaNFinais += lista[i]
+//         }
+//     }  
+//     console.log(somaNFinais) 
+//     if (somaNFinais % 2 === 0) {
+//         return 'A soma é um múltiplo de 2'
+//     } else {
+//         return 'A soma não é um múltiplo de 2'
+//     }
+// }
+// console.log(recebeValores(numeros))
+
+// //ex 6
+// let numeros2 = [1,4,6,9,11,8]
+// function recebeNumerais(lista) {
+//     let somaNIniciais = 0
+//     for (let i = 0; i <= 1; i++) {
+//         somaNIniciais += lista[i]
+//     }
+//     console.log(somaNIniciais)
+//     if (somaNIniciais % 5 === 0) {
+//         return 'A soma é um múltiplo de 5'
+//     } else {
+//         'A soma não é um múltiplo de 5'
+//     }
+// }
+// console.log(recebeNumerais(numeros2))
+
+// //ex 7
+// let nomesArray = ["Pedro", "Rafael", "José", "Carla", "Maria", "Josef", "Raquel"]
+// function recebeArrayEValores(lista, nome, numero) {
+//     let varRecebeNome = ''
+//     for (let i = 0; i < lista.length; i++) {
+//         if (i === numero) {
+//             varRecebeNome = lista[i]
+//         }
+//     }
+//     console.log(varRecebeNome)
+//     if ( varRecebeNome === nome) {
+//     return 'Acertei'
+//     } else {
+//         return 'Não é quem eu pensava'
+//     }
+// }
+// console.log(recebeArrayEValores(nomesArray, 'Rafael', 4))
+
+// //ex 8
+// let numbers1 = [1,2,3,4,5,6,10]
+// let otherNumbers = [5,7,9,4,2,3,9]
+// function recebeArrays(lista1, lista2) {
+//     let comprimento1 = lista1.length
+//     let comprimento2 = lista2.length
+//     if (comprimento1 > comprimento2) {
+//         for (let i = 0; i < lista1.length; i++){
+//             if (i = lista1.length -1) {
+//                 endPosition = lista1[i]
+//             }
+//         } 
+//         return `A maior lista é a ${lista1} cujo último número é ${endPosition}`
+//     } else {
+//         for (let i = 0; i < lista2.length; i++){
+//             if (i = lista2.length -1) {
+//                 endPosition = lista2[i]
+//             }
+//         } 
+//     }        
+//     return `A maior lista é a ${lista2} cujo último número é ${endPosition}`
+// }
+// console.log(recebeArrays(numbers1, otherNumbers))
