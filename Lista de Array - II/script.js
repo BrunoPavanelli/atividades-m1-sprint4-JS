e4cc98b4a43294dd0e0c9c0251cbb2806f2cecfe
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2022-10-20 18:13:35 -0300

    ex 4 adicionado

---
+++ Lista de Array - II/script.js
@@ -0,0 +1,52 @@
+// // const listaDeElementos = [1,5,7,9,3, "10", "11", "15"]
+// //ex 1
+// function inserirNovoELemento(num) {
+//     autput = listaDeElementos
+//     if (num >= 1 && num <=10 && num % 1 === 0) {
+//         autput.push(num)
+//     }
+//     console.log(`A lista Atualizada é ${autput}`)
+// }
+// inserirNovoELemento(2)
+
+// //ex 2
+// function retornaTodosElementos(){
+//     console.log(listaDeElementos)
+// }
+// retornaTodosElementos()
+
+// //ex 3
+// function retornaValorEspecifico(int) {
+//     const listaDeElementos = [1,5,7,9,3, "10", "11", "15"]
+//     let recebe 
+//     if (int % 1 === 0) {
+//         for (let i = 0; i < listaDeElementos.length; i++) {
+//             if (int == i) {
+//                 recebe = listaDeElementos[i]
+//             }
+//         }
+//     }
+//     return `O valor contido nesta posição ${recebe}`
+// }
+
+// console.log(retornaValorEspecifico(7))
+
+// //ex 4
+// function retornarTipoElemento(int) {
+//     const listaDeElementos = [1,5,7,9,3, "10", "11", "15"]
+//     let recebe 
+//     if (int % 1 === 0) {
+//         for (let i = 0; i < listaDeElementos.length; i++) {
+//             if (int == i) {
+//                 recebe = listaDeElementos[i]
+//             }
+//         }
+//     }
+//     if (typeof recebe === 'string') {
+//         return `O elemento ${recebe}, é um elemento do tipo texto`
+//     } else if (typeof recebe === 'number') {
+//         return `O elemento ${recebe}, é um elemento do tipo numero`
+//     }
+// }
+// console.log(retornarTipoElemento(4))
+
